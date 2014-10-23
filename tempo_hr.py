2a423392ca3aa3acd30de3a21cc2516abac828e7
stephane- <tempo@codazzi.fr>
2014-10-23 15:42:41 +0200

    Remove leaves days for calendars

--- tempo_hr.py
+++ tempo_hr.py
@@ -48,6 +48,55 @@ class tempo_hr_calc(osv.osv):
     _name = "hr.attendance"
     _inherit = 'hr.attendance'
 
+    def get_real_times(self, cr, uid, leaves, din, dout, tz):
+        res = list()
+        for l in self.pool.get('hr.holidays').browse(cr, uid, leaves):
+
+            lto = datetime.datetime\
+                .strptime(l.date_to, '%Y-%m-%d %H:%M:%S')
+            lfrom = datetime.datetime\
+                .strptime(l.date_from, '%Y-%m-%d %H:%M:%S')
+            hto = lto.strftime("%H.%M")
+            hfrom = lfrom.strftime("%H.%M")
+
+            lfrom = self.get_date(hfrom, lfrom.date(), "Etc/UTC")
+            lto = self.get_date(hto, lto.date(), "Etc/UTC")
+
+            if lfrom <= din and lto >= dout:
+                return list()
+            elif lfrom <= din and lto >= din:
+                din = lto
+            elif lfrom <= dout and lto >= dout:
+                dout = lfrom
+            elif lfrom > din and lto < dout:
+                d1 = self.get_real_times(cr, uid, leaves, din, lfrom, tz)
+                d2 = self.get_real_times(cr, uid, leaves, lto, dout, tz)
+                for d in d1:
+                    res.append(d)
+                for d in d2:
+                    res.append(d)
+                return res
+        res.append([din, dout])
+        return res
+
+    def get_date(self, hour, date, tz):
+        if tz is not False:
+            tz = pytz.timezone(tz)
+        else:
+            tz = pytz.utc
+
+        time_date = str(hour).split('.')
+        date = str(date) + " " + str(time_date[0]) + ":"
+        if len(str(time_date[1])) == 1:
+            date += "0" + str(time_date[1])
+        else:
+            date += str(time_date[1])
+        name = datetime.datetime\
+            .strptime(date, '%Y-%m-%d %H:%M')
+        date = tz.localize(name, is_dst=None)
+        date = date.astimezone(pytz.utc)
+        return date
+
     def cron_tempo_hr(self, cr, uid, context=None):
         today = datetime.datetime.now()
         employees = self.pool.get('hr.employee')\
@@ -66,60 +115,64 @@ class tempo_hr_calc(osv.osv):
                     .working_hours.attendance_ids
                 for day in working_days:
                     if int(day.dayofweek) == today.weekday():
-                        if employee.tz is not False:
-                            tz = pytz.timezone(employee.tz)
-                        else:
-                            tz = pytz.utc
 
                         holidays = self.pool.get('public.holidays.holidays')\
                             .is_holiday(cr, uid, today.date())
                         if holidays is False:
-                            # ***** SIGN IN *****
-                            time_day = str(day.hour_from).split('.')
-                            name_date = str(today.date()) + " "
-                            name_date += str(time_day[0]) + ":"
-                            if len(str(time_day[1])) == 1:
-                                name_date += "0" + str(time_day[1])
-                            else:
-                                name_date += str(time_day[1])
-                            name_date += ":00"
+                            date_in = self.get_date(
+                                day.hour_from, today.date(), employee.tz)
+                            date_out = self.get_date(
+                                day.hour_to, today.date(), employee.tz)
 
-                            name = datetime.datetime\
-                                .strptime(name_date, '%Y-%m-%d %H:%M:%S')
-                            start_date = tz.localize(name, is_dst=None)
-                            now_utc = start_date.astimezone(pytz.utc)
+                            leaves = self.pool.get('hr.holidays')\
+                                .search(cr, uid,
+                                        [('employee_id', '=', employee.id)])
+                            real_worked_time = self\
+                                .get_real_times(
+                                    cr, uid, leaves,
+                                    date_in, date_out, employee.tz)
 
-                            vals = {
-                                'action': 'sign_in',
-                                'name': now_utc,
-                                'employee_id': employee.id,
-                            }
-                            hr_attendance = self.pool.get('hr.attendance')
-                            hr_attendance.create(cr, uid, vals, context=context)
-
-                            # ***** SIGN OUT *****
-                            time_day = str(day.hour_to).split('.')
-                            name_date = str(today.date()) + " "
-                            name_date += str(time_day[0]) + ":"
-                            if len(str(time_day[1])) == 1:
-                                name_date += "0" + str(time_day[1])
-                            else:
-                                name_date += str(time_day[1])
-                            name_date += ":00"
-
-                            name = datetime.datetime\
-                                .strptime(name_date, '%Y-%m-%d %H:%M:%S')
-                            start_date = tz.localize(name, is_dst=None)
-                            now_utc = start_date.astimezone(pytz.utc)
-
-                            vals = {
-                                'action': 'sign_out',
-                                'name': now_utc,
-                                'employee_id': employee.id,
-                            }
-                            hr_attendance = self.pool.get('hr.attendance')
-                            hr_attendance.create(cr, uid, vals, context=context)
+                            for t in real_worked_time:
+                                    vals = {
+                                        'action': 'sign_in',
+                                        'name': t[0],
+                                        'employee_id': employee.id,
+                                    }
+                                    hr_attendance = self\
+                                        .pool.get('hr.attendance')
+                                    hr_attendance.create(cr, uid,
+                                                         vals, context=context)
+                                    vals = {
+                                        'action': 'sign_out',
+                                        'name': t[1],
+                                        'employee_id': employee.id,
+                                    }
+                                    hr_attendance = self\
+                                        .pool.get('hr.attendance')
+                                    hr_attendance.create(cr, uid,
+                                                         vals, context=context)
         return None
+
+    def calc_worked_hours(self, cr, uid, obj, context=None):
+
+        last_signin_id = self.search(cr, uid, [
+            ('employee_id', '=', obj.employee_id.id),
+            ('name', '<', obj.name), ('action', '=', 'sign_in')
+        ], limit=1, order='name DESC')
+        if last_signin_id:
+            last_signin = self.browse(cr, uid, last_signin_id, context=context)[0]
+            last_signin_datetime = datetime.datetime.strptime(last_signin.name, '%Y-%m-%d %H:%M:%S')
+            signout_datetime = datetime.datetime.strptime(obj.name, '%Y-%m-%d %H:%M:%S')
+            workedhours_datetime = (signout_datetime - last_signin_datetime)
+            seconds = workedhours_datetime.seconds % 60
+            minutes = (workedhours_datetime.seconds / 60) % 60
+            hours = (workedhours_datetime.seconds / 60) / 60
+            minutes_dec = float(minutes) / 60
+            seconds_dec = float(seconds) / 3600
+            res = float(hours) + float(minutes_dec) + float(seconds_dec)
+        else:
+            res = False
+        return res
 
     def _worked_hours_compute(self, cr, uid, ids, fieldnames, args, context=None):
         res = {}
@@ -127,24 +180,8 @@ class tempo_hr_calc(osv.osv):
             if obj.action == 'sign_in':
                 res[obj.id] = 0
             elif obj.action == 'sign_out':
-                # Get the associated sign-in
-                last_signin_id = self.search(cr, uid, [
-                    ('employee_id', '=', obj.employee_id.id),
-                    ('name', '<', obj.name), ('action', '=', 'sign_in')
-                ], limit=1, order='name DESC')
-                if last_signin_id:
-                    last_signin = self.browse(cr, uid, last_signin_id, context=context)[0]
-                    last_signin_datetime = datetime.datetime.strptime(last_signin.name, '%Y-%m-%d %H:%M:%S')
-                    signout_datetime = datetime.datetime.strptime(obj.name, '%Y-%m-%d %H:%M:%S')
-                    workedhours_datetime = (signout_datetime - last_signin_datetime)
-                    seconds = workedhours_datetime.seconds % 60
-                    minutes = (workedhours_datetime.seconds / 60) % 60
-                    hours = (workedhours_datetime.seconds / 60) / 60
-                    minutes_dec = float(minutes) / 60
-                    seconds_dec = float(seconds) / 3600
-                    res[obj.id] = float(hours) + float(minutes_dec) + float(seconds_dec)
-                else:
-                    res[obj.id] = False
+                res[obj.id] = self.calc_worked_hours(cr, uid,
+                                                     obj, context=context)
         return res
 
     def _calendar_start(self, cr, uid, ids, fieldnames, args, context=None):
@@ -154,10 +191,15 @@ class tempo_hr_calc(osv.osv):
                 res[obj.id] = None
             elif obj.action == 'sign_out':
                 date_stop = datetime.datetime.strptime(obj.name, '%Y-%m-%d %H:%M:%S')
-                date_delay = obj.worked_hours
+                date_delay = self.calc_worked_hours(cr, uid,
+                                                    obj, context=context)
                 hours = int(date_delay)
                 minutes = int((float(date_delay) - float(hours)) * float(60))
                 date_start = date_stop - datetime.timedelta(hours=hours, minutes=minutes)
+                print "DATES ="
+                print date_start
+                print date_stop
+                print date_delay
                 res[obj.id] = date_start
         return res
 
@@ -170,6 +212,55 @@ class tempo_hr_calc(osv.osv):
 class tempo_hr_plan(osv.osv):
     _name = "tempo_hr"
 
+    def get_real_times(self, cr, uid, leaves, din, dout, tz):
+        res = list()
+        for l in self.pool.get('hr.holidays').browse(cr, uid, leaves):
+
+            lto = datetime.datetime\
+                .strptime(l.date_to, '%Y-%m-%d %H:%M:%S')
+            lfrom = datetime.datetime\
+                .strptime(l.date_from, '%Y-%m-%d %H:%M:%S')
+            hto = lto.strftime("%H.%M")
+            hfrom = lfrom.strftime("%H.%M")
+
+            lfrom = self.get_date(hfrom, lfrom.date(), "Etc/UTC")
+            lto = self.get_date(hto, lto.date(), "Etc/UTC")
+
+            if lfrom <= din and lto >= dout:
+                return list()
+            elif lfrom <= din and lto >= din:
+                din = lto
+            elif lfrom <= dout and lto >= dout:
+                dout = lfrom
+            elif lfrom > din and lto < dout:
+                d1 = self.get_real_times(cr, uid, leaves, din, lfrom, tz)
+                d2 = self.get_real_times(cr, uid, leaves, lto, dout, tz)
+                for d in d1:
+                    res.append(d)
+                for d in d2:
+                    res.append(d)
+                return res
+        res.append([din, dout])
+        return res
+
+    def get_date(self, hour, date, tz):
+        if tz is not False:
+            tz = pytz.timezone(tz)
+        else:
+            tz = pytz.utc
+
+        time_date = str(hour).split('.')
+        date = str(date) + " " + str(time_date[0]) + ":"
+        if len(str(time_date[1])) == 1:
+            date += "0" + str(time_date[1])
+        else:
+            date += str(time_date[1])
+        name = datetime.datetime\
+            .strptime(date, '%Y-%m-%d %H:%M')
+        date = tz.localize(name, is_dst=None)
+        date = date.astimezone(pytz.utc)
+        return date
+
     def cron_plan_tempo_hr(self, cr, uid, context=None):
         hr_tempo = self.pool.get('tempo_hr')
         ids = hr_tempo.search(cr, uid, [], context=context)
@@ -180,16 +271,12 @@ class tempo_hr_plan(osv.osv):
                     ['|', ('active', '=', False),
                      ('active', '=', True)])
         for employee in self.pool.get('hr.employee').browse(cr, uid, employees):
-            print employee.name
             working_days = employee.contract_id\
                 .working_hours.attendance_ids
             year_now = datetime.datetime.now().year
             current_date = datetime.date(year_now, 1, 1)
             date_stop = datetime.date(year_now + 1, 12, 31)
-            if employee.tz is not False:
-                tz = pytz.timezone(employee.tz)
-            else:
-                tz = pytz.utc
+
             if working_days is not None:
                 while current_date <= date_stop:
                     holidays = self.pool.get('public.holidays.holidays')\
@@ -197,42 +284,28 @@ class tempo_hr_plan(osv.osv):
                     if holidays is False:
                         for day in working_days:
                             if int(day.dayofweek) == current_date.weekday():
-                                time_in = str(day.hour_from).split('.')
-                                date_in = str(current_date) + " "
-                                date_in += str(time_in[0]) + ":"
-                                if len(str(time_in[1])) == 1:
-                                    date_in += "0" + str(time_in[1])
-                                else:
-                                    date_in += str(time_in[1])
-                                name_in = datetime.datetime\
-                                    .strptime(date_in, '%Y-%m-%d %H:%M')
-                                date_in = tz.localize(name_in, is_dst=None)
-                                dateIn = date_in.astimezone(pytz.utc)
+                                date_in = self.get_date(
+                                    day.hour_from, current_date, employee.tz)
+                                date_out = self.get_date(
+                                    day.hour_to, current_date, employee.tz)
 
-                                time_out = str(day.hour_to).split('.')
-                                date_out = str(current_date) + " "
-                                date_out += str(time_out[0]) + ":"
-                                if len(str(time_out[1])) == 1:
-                                    date_out += "0" + str(time_out[1])
-                                else:
-                                    date_out += str(time_out[1])
-                                name_out = datetime.datetime\
-                                    .strptime(date_out, '%Y-%m-%d %H:%M')
-                                date_out = tz.localize(name_out, is_dst=None)
-                                dateOut = date_out.astimezone(pytz.utc)
+                                leaves = self.pool.get('hr.holidays')\
+                                    .search(cr, uid,
+                                            [('employee_id', '=', employee.id)])
+                                real_worked_time = self\
+                                    .get_real_times(
+                                        cr, uid, leaves,
+                                        date_in, date_out, employee.tz)
 
-                                print "DATe : "
-                                print dateIn
-                                print dateOut
-                                vals = {
-                                    'employee_id': employee.id,
-                                    'date_start': dateIn,
-                                    'date_stop': dateOut,
-                                }
-                                hr_tempo = self.pool.get('tempo_hr')
-                                hr_tempo.create(cr, uid, vals, context=context)
+                                for t in real_worked_time:
+                                    vals = {
+                                        'employee_id': employee.id,
+                                        'date_start': t[0],
+                                        'date_stop': t[1],
+                                    }
+                                    hr_tempo = self.pool.get('tempo_hr')
+                                    hr_tempo.create(cr, uid, vals, context=context)
                     current_date = current_date + datetime.timedelta(days=1)
-
         return None
 
     _columns = {
